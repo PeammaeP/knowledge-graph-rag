fc23e7f293d3cf9e37410a65c1ad00df505cd67a
PeamMiiZ <peemmygg@gmail.com>
2025-11-03 16:44:33 +0700

    additional refactor in src/main.py and src/database/vectordb.py

--- src/database/vectordb.py
+++ src/database/vectordb.py
@@ -1,7 +1,10 @@
 from neo4j import GraphDatabase
 
-driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", "password"))
+def get_graph_database():
+    driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", "password"))
 
-driver.execute_query("""CREATE VECTOR INDEX pdf IF NOT EXISTS
-FOR (c:Chunk)
-ON c.embedding""")
+    driver.execute_query("""CREATE VECTOR INDEX pdf IF NOT EXISTS
+    FOR (c:Chunk)
+    ON c.embedding""")
+
+    return driver
--- src/main.py
+++ src/main.py
@@ -1,18 +1,35 @@
 from src.utils.chunks import chunk_text
 from src.utils.getfile import get_text_from_file
 from src.utils.embedding import embedding
+from src.database.vectordb import get_graph_database
 
-if __name__ == "__main__":
+class Config:
+    # File Configuration
     remote_pdf_url = "https://arxiv.org/pdf/1709.00666.pdf"
     pdf_filename = "ch02-downloaded.pdf"
 
+    # Chunking Hyperparameter
     chunk_size = 500
     overlap = 40
 
-    text = get_text_from_file(remote_pdf_url, pdf_filename)
+    # Cypher Query Template
+    cypher_query = '''
+    WITH $chunks as chunks, range(0, size($chunks)) AS index
+    UNWIND index AS i
+    WITH i, chunks[i] AS chunk, $embeddings[i] AS embedding
+    MERGE (c:Chunk {index: i})
+    SET c.text = chunk, c.embedding = embedding
+    '''
 
-    chunks = chunk_text(text, chunk_size, overlap)
+if __name__ == "__main__":
+    config = Config()
+
+    text = get_text_from_file(config.remote_pdf_url, config.__init_subclass__)
+
+    chunks = chunk_text(text, config.chunk_size, config.overlap)
 
     embeddings = embedding(chunks)
+    #print(len(chunks))
 
-    print(len(chunks))
+    driver = get_graph_database()
+    driver.execute_query(config.cypher_query, chunks=chunks, embeddings=embeddings)
